b5d284e3719b2f2a6352b6503d83a61237ec67f3
vaidehijoshi <vaidehi.sj@gmail.com>
2014-10-27 16:10:06 -0400

    throwaway

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,7 +1,5 @@
 class RootController < ApplicationController
   
-  resources :index
-
   get '/' do
     # erb :index
     erb :homepage
@@ -12,18 +10,13 @@ class RootController < ApplicationController
   end
 
   get '/index.html' do
-    erb :index
+    `open './app/views/index.html.erb'`
   end
 
   post '/make-magic' do 
     run_session = LinkedInSession.new
     @s = run_session.work_magic
     erb :magic_display
-  end
-
-  get '/flatiron_alums' do
-    # @linkedin_scraper = InfoScraper.new
-    erb :flatiron_alums
   end
 
 end
